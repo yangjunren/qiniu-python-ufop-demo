372dad4765c2deccf907d7d72d56bfce99271161
yjr18809483524 <2217757794@qq.com>
2021-01-26 14:36:38 +0800

    update ufopdemo

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.6
 ONBUILD ADD . /usr/src/qiniu-python-ufop-demo
 ONBUILD RUN /usr/local/bin/python-build --dir /usr/src/qiniu-python-ufop-demo
-ENTRYPOINT /usr/src/qiniu-python-ufop-demo
+ONBUILD RUN pip install -r requirements.txt
+CMD python server.py
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-import os
+# -*- coding: utf-8 -*-
+# flake8: noqa
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging, urllib.request, json, base64
+import logging, urllib.request, base64
 from urllib.parse import urlparse, parse_qsl
 
 # HTTPGetMaxSize 最大处理的文件长度
@@ -14,11 +15,6 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def _err_response(self):
-        self.send_response(404)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def _err_400_response(self):
         self.send_response(400)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
@@ -31,52 +27,37 @@ class S(BaseHTTPRequestHandler):
             self.wfile.write("health is {}".format("OK").encode("utf-8"))
         else:
             self._err_response()
-            self.wfile.write("request url 404！".encode("utf-8"))
+            self.wfile.write("request error！".encode("utf-8"))
 
     # post请求，用于处理handler请求，并返回处理结果
     def do_POST(self):
         ret = urlparse(self.path)
         if ret.path == "/handler":
             ret_query = dict(parse_qsl(ret.query))
-            if len(ret_query) == 2 and ret_query["url"] != "":
+            if ret_query['cmd'] == "yjr_ufoptest" and ret_query['url'] != "":
                 try:
                     url = ret_query["url"]
                     f = urllib.request.urlopen(url)
                     body = f.read()
-                    print(type(body))
                     data = base64.b64encode(body).decode("utf-8")
                     self._set_response()
                     self.wfile.write("POST request success！\n\n{0}".format(data).encode("utf-8"))
+                    logging.info(
+                        "POST request,\nPath: {0}\nHeaders:\n{1}\n\nBody:\n{2}\n".format(str(self.path),
+                                                                                         str(self.headers), data))
                 except Exception as err:
                     logging.info(err)
-                logging.info(
-                    "POST request,\nPath: {0}\nHeaders:\n{1}\n\nBody:\n{2}\n".format(str(self.path), str(self.headers),
-                                                                                     data))
-            elif len(ret_query) < 2:
-                try:
-                    content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-                    data = self.rfile.read(content_length).decode("utf-8")  # <--- Gets the data itself
-                    if json.loads(data)['data']:
-                        self._set_response()
-                        self.wfile.write("POST request success!\n\n{0}".format(data).encode("utf-8"))
-                    else:
-                        self._err_400_response()
-                        self.wfile.write("request error！".encode("utf-8"))
-                except Exception as err:
-                    self._err_400_response()
-                    logging.info(err)
-                logging.info(
-                    "POST request,\nPath: {0}\nHeaders:\n{1}\n\nBody:\n{2}\n".format(str(self.path), str(self.headers),
-                                                                                     data))
+                    self._err_response()
+                    self.wfile.write("request error！".encode("utf-8"))
             else:
-                self._err_400_response()
+                self._err_response()
                 self.wfile.write("request error！".encode("utf-8"))
         else:
-            self._err_400_response()
+            self._err_response()
             self.wfile.write("request error！".encode("utf-8"))
 
 
-def run(server_class=HTTPServer, handler_class=S, port=9200):
+def run(server_class=HTTPServer, handler_class=S, port=9100):
     logging.basicConfig(level=logging.INFO)
     server_address = ('0.0.0.0', port)
     httpd = server_class(server_address, handler_class)
